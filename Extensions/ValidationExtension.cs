beeddba25545828ab575bc6bd7e2216ecb0ceab1
nucanasaraia <nucanasaraia@gmail.com>
2026-04-15 16:42:03 +0400

    feat: implement validation

--- Extensions/ValidationExtension.cs
+++ Extensions/ValidationExtension.cs
@@ -1,10 +1,17 @@
-﻿namespace EmergencyNotifRespons.Extensions
+﻿using EmergencyNotifRespons.Validations;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
+namespace EmergencyNotifRespons.Extensions
 {
     public static class ValidationExtension
     {
         public static void ConfigureValidation(this IServiceCollection services)
         {
-
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblyContaining<RegisterValidator>();
+            services.AddValidatorsFromAssemblyContaining<LoginValidator>();
+            services.AddValidatorsFromAssemblyContaining<PasswordResetValidator>();
         }
     }
 }
---
+++ Validations/LoginValidator.cs
@@ -0,0 +1,17 @@
+﻿using EmergencyNotifRespons.Requests;
+using FluentValidation;
+
+namespace EmergencyNotifRespons.Validations
+{
+    public class LoginValidator : AbstractValidator<LogInRequest>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("Username is required");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required");
+        }
+    }
+}
